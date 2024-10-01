e273e73ff615e6850e6d8ce7d760a6e3543e035b
Lucas Henrique Haboski da Silva <lucas.hhaboski@gmail.com>
2024-10-01 10:05:25 -0300

    Create exercicio01.c

---
+++ Exercicios/alocacaoDinamica02/exercicio01.c
@@ -0,0 +1,3 @@
+// Desenvolva um programa que calcule a soma de duas matrizes M x N de números reais (double).
+// A implementação deste programa deve considerar as dimensões fornecida pelo usuário.
+
